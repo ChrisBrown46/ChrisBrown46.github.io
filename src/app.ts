c3f925d1d6272fa43a3f61bb9d62bc2f7616c988
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2019-08-01 14:59:39 -0600

    Added routing and migrated SCSS to a `styles` folder

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-export class App { }
+import { PLATFORM } from "aurelia-pal";
+import { RouterConfiguration, Router } from "aurelia-router";
+
+export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+
+    config.title = "Aurelia";
+    config.map([
+      { route: "", redirect: "projects" },
+      { route: "projects", name: "projects", moduleId: PLATFORM.moduleName("./projects/projects"), nav: false, title: "Projects" },
+      { route: "resume", name: "resume", moduleId: PLATFORM.moduleName("./resume/resume"), nav: false, title: "Resume" },
+      { route: "about", name: "about", moduleId: PLATFORM.moduleName("./about/about"), nav: false, title: "About" },
+    ]);
+
+    config.mapUnknownRoutes({ route: "", redirect: "projects" });
+  }
+}
---
+++ src/resume/resume.ts
@@ -0,0 +1,7 @@
+export class Resume {
+  message: string;
+
+  constructor() {
+    this.message = "Here is my resume!";
+  }
+}
