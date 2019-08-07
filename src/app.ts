af5661e1681bdc59feb4d32ca2bc97584c2a528d
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2019-08-07 10:47:28 -0600

    Removed extra CSS acting on columns

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export class App {
 
     config.title = "Aurelia";
     config.map([
-      { route: "", redirect: "projects" },
+      { route: "", redirect: "portfolio" },
       { route: "portfolio", name: "portfolio", moduleId: PLATFORM.moduleName("./portfolio/portfolio"), nav: false, title: "Portfolio" },
       { route: "resume", name: "resume", moduleId: PLATFORM.moduleName("./resume/resume"), nav: false, title: "Resume" },
       { route: "about", name: "about", moduleId: PLATFORM.moduleName("./about/about"), nav: false, title: "About" },
