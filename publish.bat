84c1314f20075eeb0cb66a27ab269dcb081d719e
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2019-08-07 11:31:47 -0600

    GitHub pages update

--- publish.bat
+++ publish.bat
@@ -5,6 +5,9 @@ git push && ^
 git checkout master && ^
 git checkout website dist && ^
 move dist\* . && ^
+mkdir css && ^
+move dist\css\* css\ && ^
+rmdir dist\css && ^
 rmdir dist && ^
 git add . && ^
 git commit -m "GitHub pages update" && ^
